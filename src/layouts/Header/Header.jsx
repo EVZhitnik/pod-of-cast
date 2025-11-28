588caef115814c1d45c2175ff8b38fd270bdff92
Evgeniy Zhitnik <jsi.zhitnik@gmail.com>
2025-11-28 14:04:08 +0300

    feat: настройка глобальной обёртки
    — создание Header, Content, Footer
    — создание глобальной обёртки global

---
+++ src/layouts/Header/Header.jsx
@@ -0,0 +1,15 @@
+
+import './Header.scss';
+
+
+const Header = (props) => {
+  const {
+    url,
+  } = props;
+
+  return (
+    <header>HEADER</header>
+  )
+}
+
+export default Header;
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,11 @@
 export const metadata = {
-  title: 'Home',
+  title: 'About',
 }
 
 export default () => {
   return (
     <>
-      <h1>Home</h1>
+      <h1>About</h1>
     </>
   )
 }
