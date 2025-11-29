252fa6224cd12859130005981f062b0f88832945
Evgeniy Zhitnik <jsi.zhitnik@gmail.com>
2025-11-29 18:28:55 +0300

    feat: реализация header
    — создание компонентов Logo, Icon, Button, BurgerButton
    — реализация модуля OverlayMenu через JS-логику

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,14 +1,72 @@
-
+import Logo from '@/components/Logo';
 import './Header.scss';
-
+import classNames from 'classnames';
+import Button from '@/components/Button';
+import BurgerButton from '@/components/BurgerButton';
 
 const Header = (props) => {
   const {
     url,
   } = props;
 
+  const menuItems = [
+    {
+      label: 'Episodes',
+      href: './',
+    },
+    {
+      label: 'About',
+      href: './about',
+    },
+    {
+      label: 'More',
+      href: './more',
+      submenu: <Button className="header__button-submenu" mode='submenu' label="Submenu" isLabelHidden iconName="header-arrow-down"/>
+    },
+  ]
+
   return (
-    <header>HEADER</header>
+    <header className="header" data-js-overlay-menu="">
+      <div className="header__inner container">
+        <Logo className="header__logo"/>
+        <dialog className="header__overlay-menu-dialog" data-js-overlay-menu-dialog="">
+          <nav className="header__menu">
+            <ul className="header__menu-list">
+              {menuItems.map(({label, href, submenu}, index) => (
+                <li className={classNames("header__menu-item", {'has-submenu': submenu})} key={index}>
+                  <a 
+                    className={classNames("header__menu-link", {'is-active': href === '.' + url})} 
+                    href={href}
+                  >
+                    {label}
+                  </a>
+                  {submenu && submenu}
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <div className="header__actions">
+            <Button
+              className="header__button-recent"
+              href="/"
+              label="Recent Episodes"
+              mode="recent"
+            />
+            <Button
+              className="header__button-subscribe"
+              href="/"
+              label="Subscripe"
+              mode="subscripe"
+            />
+          </div>
+        </dialog>
+        <BurgerButton 
+          className="header__burger-button visible-mobile" 
+          extraAttrs ={{
+            'data-js-overlay-menu-burger-button': ''
+          }}/>
+      </div>
+    </header>
   )
 }
 
