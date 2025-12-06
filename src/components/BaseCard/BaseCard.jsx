ec3e9e62f511d66d2d3ed82a887473909d11c4fd
Evgeniy Zhitnik <jsi.zhitnik@gmail.com>
2025-12-06 21:28:43 +0300

    feat: реализация секции News
    — создание компонента BaseCard

---
+++ src/components/BaseCard/BaseCard.jsx
@@ -0,0 +1,102 @@
+import classNames from 'classnames';
+import './BaseCard.scss';
+import { Image } from 'minista';
+import IconLink from '@/components/IconLink';
+import Tags from '../Tags';
+
+const BaseCard = (props) => {
+  const {
+    className,
+    mode = '',
+    dataCard = [],
+    isTitleTheLink = false,
+    isSubscription = false,
+  } = props;
+
+  const socialsData = [
+    {
+      label: "Tiktok",
+      iconName: "tiktok-base-card",
+    },
+    {
+      label: "Twitter",
+      iconName: "twitter-base-card",
+    },
+    {
+      label: "Instagram",
+      iconName: "instagram-base-card",
+    },
+  ];
+
+  return (
+    <div className={className}>
+        {dataCard.map((dataCardItems, index) => {
+          const {
+            imgSrc,
+            subtitle,
+            title,
+            description,
+            tags = [],
+            date,
+            sub,
+          } = dataCardItems;
+
+          const textImage = isTitleTheLink 
+            ? `Read more about: ${title}`
+            : `Photo by ${title}`
+          ;
+
+          return (
+            <article className={classNames('base-card', {
+              [`base-card--${mode}`]: mode,
+            })} key={index}>
+              <Image
+                className="base-card__image" 
+                src={imgSrc} 
+                alt={textImage}
+              />
+              <div className="base-card__text">
+                <span className="base-card__subtitle">{subtitle}</span>
+                {isTitleTheLink ? (
+                  <h3 className="base-card__title">
+                    <a className="base-card__link" href="./">{title}</a>
+                  </h3>
+                ) : (
+                  <h3 className="base-card__title">{title}</h3>
+                )}
+                <div className="base-card__description">{description}</div>
+              </div>
+              {isSubscription ? (
+                <div className="base-card__subscription">
+                  <span className="base-card__subscription-text">{sub}</span>
+                  <div className="base-card__subscription-soc1als">
+                    {socialsData.map(({ label, iconName }, index) => (
+                      <IconLink 
+                        className="base-card__subscription-soc1als-link"
+                        mode="base-card"
+                        title={label}
+                        iconName={iconName}
+                        width={20}
+                        height={20}
+                        key={index}                      
+                      />
+                    ))}
+                  </div>
+                </div>
+              ) : (
+                <div className="base-card__meta">
+                  <Tags 
+                    className="base-card__meta-tags"
+                    items={tags}
+                  />
+                  <div className="base-card__meta-date">{date}</div>
+                </div>
+              )}
+            </article>
+          )
+        })}
+    </div>
+  );
+};
+
+export default BaseCard;
---
+++ src/sections/News/News.jsx
@@ -0,0 +1,47 @@
+import Section from '@/layouts/Section';
+import './News.scss';
+import BaseCard from '@/components/BaseCard';
+import Button from '@/components/Button';
+
+const News = () => {
+  const dataBaseCard = [
+    {
+      imgSrc: './src/assets/images/base-card/1.jpg',
+      subtitle: 'Podcast',
+      title: 'Setup Your Own Podcast',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimvel iam, quis nostrud exercitation ullamco laboris...',
+      tags: ['business', 'podcast', 'startup'],
+      date: <><time datetime="2021-09-14">Sep 14, 2021</time></>
+    },
+    {
+      imgSrc: './src/assets/images/base-card/2.jpg',
+      subtitle: 'Tips & Trick',
+      title: 'Doodle artwork 101',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minimvel iam, quis nostrud exercitation ullamco laboris...',
+      tags: ['art', 'creative', 'tips and trick'],
+      date: <><time datetime="2021-09-12">Sep 12, 2021</time></>
+    },
+  ];
+
+  return (
+    <Section
+      title="Article and News"
+      titleId="news-title"
+      description="News, tips, tricks and more"
+    >
+      <BaseCard 
+        className="news__card"
+        mode="news"
+        dataCard={dataBaseCard}
+        isTitleTheLink={true}
+      />
+      <Button 
+        className="news-card__link"
+        href="/"
+        label="Browse all"
+      />
+    </Section>
+  );
+};
+
+export default News;
