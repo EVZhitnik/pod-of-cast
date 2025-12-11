5810652eb09f9a4d173c8db4935c795f24489dd8
Evgeniy Zhitnik <jsi.zhitnik@gmail.com>
2025-12-11 20:29:42 +0300

    feat: реализация секции hero для страницы single-blog

---
+++ src/pages/single.jsx
@@ -0,0 +1,13 @@
+import Hero from "@/sections/Hero";
+
+export const metadata = {
+  title: 'Single Blog',
+};
+
+export default () => {
+  return (
+    <>
+      <Hero label="single-blog" />
+    </>
+  )
+};
