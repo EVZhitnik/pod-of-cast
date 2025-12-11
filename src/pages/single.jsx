f559c080ad9ad44be54d0d4d714de3eaf5354fae
Evgeniy Zhitnik <jsi.zhitnik@gmail.com>
2025-12-11 22:25:46 +0300

    feat: добавление секции Pod

--- src/pages/single.jsx
+++ src/pages/single.jsx
@@ -1,6 +1,7 @@
 import Hero from "@/sections/Hero";
 import Blog from "@/sections/Blog";
 import Related from "@/sections/Related";
+import Pod from "@/sections/Pod";
 
 export const metadata = {
   title: 'Single Blog',
@@ -12,6 +13,7 @@ export default () => {
       <Hero label="single-blog" />
       <Blog />
       <Related />
+      <Pod />
     </>
   )
 };
